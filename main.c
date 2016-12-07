7a42446b968e2643269b8ec310011f009484dcf5
hikalium <hikalium@hikalium.com>
2016-12-07 23:49:51 +0900

    save commit

--- main.c
+++ main.c
@@ -10,6 +10,23 @@
 
 #define INDEX_BUF_SIZE	(20 * INPUT_LINE_SIZE)
 
+typedef struct {
+	char *str;			// segbuf中へのポインタ
+	int len;
+	char isFixed;
+	int duplicateCount;
+} Segment;
+
+typedef struct {
+	// T'
+	int tLen;
+	char tStr[INPUT_LINE_SIZE];
+	// S
+	int segCount;
+	Segment *segList[MAX_SEGMENTS];
+	Segment segListBase[MAX_SEGMENTS];
+	char segBuf[2 * INPUT_LINE_SIZE]; 		// セグメントの実体をここに詰め込む。
+} InputData;
 
 typedef struct {
 	int segID;
@@ -17,33 +34,89 @@ typedef struct {
 	int *candidateOfsList;	// -1: 終端
 } SegTag;
 
+InputData givenData;
+//char tbuf[INPUT_LINE_SIZE];
+//int tlen;
+//char segbuf[2 * INPUT_LINE_SIZE];	// セグメントの実体をここに詰め込む。
+//char *segList[MAX_SEGMENTS];		// ここはsegbuf中へのポインタしかもたない。この添え字をsegIDとする。
+//int segLenList[MAX_SEGMENTS];
+//int segCount = 0;
+
+//char fixedStr[INPUT_LINE_SIZE];		// 修正後の文字列が入る。初めは0初期化されている。
+
+//int segFixedOfs[MAX_SEGMENTS];		// 該当するセグメントが配置されたオフセットを保存。-1に初期化される。
+
+//SegTag segDecisionListBuf[MAX_SEGMENTS];	// この配列の添え字はsegIDではない！！！
+//SegTag* segDecisionList[MAX_SEGMENTS];		// この配列の添え字はsegIDではない！！！
+//SegTag* segTagMap[MAX_SEGMENTS];			// この配列の添え字はsegIDです。
+
+//int candidateOfsBuf[MAX_CANDIDATE_OFFSETS];
+//int candidateOfsBufCount = 0;
+
+//int *indexTable[27];
+//int indexBuf[INDEX_BUF_SIZE];
+//int indexBufCount = 0;
+
+//
+//	データ読み込み
+//
 int seglen_cmp(const void *p, const void *q)
 {
-	return (strlen(*(const char **)q) - strlen(*(const char **)p));
+	return (strlen((*(const Segment **)q)->str) - strlen((*(const Segment **)p)->str));
 }
 
-char tbuf[INPUT_LINE_SIZE];
-int tlen;
-char segbuf[2 * INPUT_LINE_SIZE];	// セグメントの実体をここに詰め込む。
-char *segList[MAX_SEGMENTS];		// ここはsegbuf中へのポインタしかもたない。この添え字をsegIDとする。
-int segLenList[MAX_SEGMENTS];
-int segCount = 0;
+void readT()
+{
+	// 読み込み
+	fgets(givenData.tStr, INPUT_LINE_SIZE, stdin);
+	givenData.tLen = strlen(givenData.tStr);
+	givenData.tLen--;
+	givenData.tStr[givenData.tLen] = 0;	// 末尾の改行文字を除去
 
-char fixedStr[INPUT_LINE_SIZE];		// 修正後の文字列が入る。初めは0初期化されている。
+	// 以下はデバッグ用
+	fprintf(stderr, "T'[%d]=%s\n", givenData.tLen, givenData.tStr);
+}
 
-int segFixedOfs[MAX_SEGMENTS];		// 該当するセグメントが配置されたオフセットを保存。-1に初期化される。
+void readSegList()
+{
+	int i, k;
+	Segment *s;
+	char *p = givenData.segBuf;
+	for(i = 0; i < MAX_SEGMENTS; i++){
+		s = &givenData.segListBase[i];
+		if(!fgets(p, INPUT_LINE_SIZE, stdin)) break;
+		s->len = strlen(p) - 1;
+		p[s->len] = 0;
+		//
+		for(k = 0; k < i; k++){
+			if(strcmp(p, givenData.segListBase[k].str) == 0) break;
+		}
+		if(k < i){
+			// 重複を発見。
+			givenData.segListBase[k].duplicateCount++;
+			i--;
+			continue;
+		}
+		// 新規タグなので追加
+		s->str = p;
+		s->isFixed = 0;
+		givenData.segList[i] = s;
+		s->duplicateCount = 1;
+		p += s->len + 1;
+		givenData.segCount++;
+	}
 
-SegTag segDecisionListBuf[MAX_SEGMENTS];	// この配列の添え字はsegIDではない！！！
-SegTag* segDecisionList[MAX_SEGMENTS];		// この配列の添え字はsegIDではない！！！
-SegTag* segTagMap[MAX_SEGMENTS];			// この配列の添え字はsegIDです。
+	qsort(givenData.segList, givenData.segCount, sizeof(Segment *), seglen_cmp);
 
-int candidateOfsBuf[MAX_CANDIDATE_OFFSETS];
-int candidateOfsBufCount = 0;
-
-int *indexTable[27];
-int indexBuf[INDEX_BUF_SIZE];
-int indexBufCount = 0;
-
+	for(i = 0; i < givenData.segCount; i++){
+		s = givenData.segList[i];
+		fprintf(stderr, "S%04d[%2d]x%3d = %s\n", i, s->len, s->duplicateCount, s->str);
+	}
+	fprintf(stderr, "Given segs: %d\n", givenData.segCount);
+	//segCount = i;	// MIN_SEG_LEN文字以下は検査しても精度があがらないのでさようなら
+	//fprintf(stderr, "Segs longer than %d: %d\n", MIN_SEG_LEN, segCount);
+}
+/*
 int segtag_cmp(const void *p, const void *q)
 {
 	const SegTag *sp = *(const SegTag **)p, *sq = *(const SegTag **)q;
@@ -51,7 +124,8 @@ int segtag_cmp(const void *p, const void *q)
 	if(d) return d;
 	return (segLenList[sq->segID] - segLenList[sp->segID]);
 }
-
+*/
+/*
 int is_matched(int ofs, int segID)
 {
 	// segIDがofsに配置できるなら1を返す。矛盾する場合は0を返す。
@@ -69,20 +143,21 @@ void putSegAtOfs(int segID, int ofs)
 	strncpy(&fixedStr[ofs], segList[segID], segLenList[segID]);
 	//fprintf(stderr, "S%04d[%02d] -> Fixed %d\n", segID, segLenList[segID], ofs);
 }
-
-void fillRestX()
+*/
+void fillRestX(char *fixedStr)
 {
 	int i;
 	// まず、そもそも与えられている正解部分を再度上書きする（正解をわざわざ間違える必要なんてない）
-	for(i = 0; i < tlen; i++){
-		if(tbuf[i] != 'x') fixedStr[i] = tbuf[i];
+	for(i = 0; i < givenData.tLen; i++){
+		if(givenData.tStr[i] != 'x') fixedStr[i] = givenData.tStr[i];
 	}
 
-	for(i = 0; i < tlen; i++){
+	for(i = 0; i < givenData.tLen; i++){
 		if(!fixedStr[i]) fixedStr[i] = 'c';
 	}
 }
 
+/*
 #define GET_KEY(s)	((s[0] - 'a') + (s[1] - 'a') * 3 + (s[2] - 'a') * 9)
 #define IS_KEY_OFS(s, i, k) ( \
 	(s[i] == 'x' || s[i] - 'a' == k % 3) && \
@@ -103,71 +178,20 @@ void generateIndex()
 		indexBuf[indexBufCount++] = -1;
 	}
 	// 以下はデバッグ用
-/*
-	for(k = 0; k < 9; k++){
-		fprintf(stderr, ">>> %c%c:\n", k%3 + 'a', k/3 + 'a');
-		for(i = 0; ~indexTable[k][i]; i++){
-			fprintf(stderr, "%s\n", &tbuf[indexTable[k][i]]);
-		}
-	}
-*/
+
+//	for(k = 0; k < 9; k++){
+//		fprintf(stderr, ">>> %c%c:\n", k%3 + 'a', k/3 + 'a');
+//		for(i = 0; ~indexTable[k][i]; i++){
+//			fprintf(stderr, "%s\n", &tbuf[indexTable[k][i]]);
+//		}
+//	}
+
 	fprintf(stderr, "IndexBuf used: %d / %d (%.6f)\n", indexBufCount, INDEX_BUF_SIZE, (double)indexBufCount / INDEX_BUF_SIZE);
 
 }
-
-void readT()
-{
-	// 読み込み
-	fgets(tbuf, INPUT_LINE_SIZE, stdin);
-	tlen = strlen(tbuf);
-	tlen--;
-	tbuf[tlen] = 0;
-
-	// 索引を作成
-	generateIndex();
-
-	// 以下はデバッグ用
-	fprintf(stderr, "T'[%d]=%s\n", tlen, tbuf);
-/*
-	int k;
-	for(i = 0; i < 8; i++){
-		for(k = 0; k < i * 2; k++){
-			fputc(' ', stderr);
-		}
-		for(k = 0; k < 8; k++){
-			fputc('0' + ((tHeaderList[i] >> (7 - k)) & 1), stderr);
-		}
-		fputc('\n', stderr);
-	}
 */
-}
 
-void readSegList()
-{
-	int i, len;
-	char *p = segbuf;
-	for(i = 0; i < MAX_SEGMENTS; i++){
-		segList[i] = p;
-		segFixedOfs[i] = -1;
-		if(!fgets(segList[i], INPUT_LINE_SIZE, stdin)) break;
-		len = strlen(segList[i]) - 1;
-		segList[i][len] = 0;
-		//
-		p += len + 1;
-		segCount++;
-	}
-
-	qsort(segList, segCount, sizeof(char *), seglen_cmp);
-
-	for(i = 0; i < segCount; i++){
-		segLenList[i] = strlen(segList[i]);
-		if(segLenList[i] < MIN_SEG_LEN) break;
-	}
-	fprintf(stderr, "Given segs: %d\n", segCount);
-	segCount = i;	// MIN_SEG_LEN文字以下は検査しても精度があがらないのでさようなら
-	fprintf(stderr, "Segs longer than %d: %d\n", MIN_SEG_LEN, segCount);
-}
-
+/*
 int get_num_of_seg_ofs_puttable(SegTag *st)
 {
 	// すでに配置されているなら-1を返す
@@ -261,17 +285,11 @@ void showDecisionList()
 			segDecisionList[i]->candidates,
 			segList[segDecisionList[i]->segID]
 		);
-		/*
-		int k;
-		for(k = 0; segDecisionList[i]->candidateOfsList[k] != -1; k++){
-			fprintf(stderr, "%d ", segDecisionList[i]->candidateOfsList[k]);
-		}
-		fprintf(stderr, "\n");
-		*/
 	}
 	
 }
-
+*/
+/*
 int putDecidedSeg()
 {
 	// 配置しうるofsが一つしかないsegを配置してしまう。
@@ -317,6 +335,7 @@ void putAllDecidedSeg()
 		if(fixCount == 0) break;
 	}
 }
+*/
 /*
 void fill_nkgwer_sub(int segID)
 {
@@ -336,6 +355,7 @@ void fill_nkgwer_sub(int segID)
 	}
 }
 */
+/*
 void fill_nkgwer_sub(int segID)
 {
 	// nkgwer's algorithm
@@ -351,21 +371,9 @@ void fill_nkgwer_sub(int segID)
 			return;
 		}
 	}
-/*
-
-	for(ofs = 0; ofs < tlen; ofs++){
-		if(seglen > (tlen - ofs)) continue;
-		if(is_matched(ofs, segID)){
-			putSegAtOfs(segID, ofs);
-			if(seglen > 40){
-				fprintf(stderr, "%s\n","0ver 40");
-				return;
-			}
-		}
-	}
-*/	
 }
-
+*/
+/*
 void fill_nkgwer()
 {
 	// nkgwer's algorithm
@@ -390,13 +398,21 @@ void popFixedStr()
 		if(fixedStrStack[i]) fixedStr[i] = fixedStrStack[i];
 	}
 }
+*/
 
+
+char fixedStr[INPUT_LINE_SIZE];		// 修正後の文字列が入る。初めは0初期化されている。
 int main_prg(int argc, char** argv)
 {
-	// 読み込み
+	// T'読み込み
 	readT();
-	readSegList();
 
+	// 索引を作成
+//	generateIndex();
+	// S読み込み
+
+	readSegList();
+/*
 	//
 	initDecisionList();
 	showDecisionList();
@@ -409,13 +425,15 @@ int main_prg(int argc, char** argv)
 	pushFixedStr();
 
 	// 後処理
-	fillRestX();	// 埋められなかった部分をなんとかする
+*/
+	fillRestX(fixedStr);	// 埋められなかった部分をなんとかする
+
 	// 結果出力
 	printf("%s\n", fixedStr);
-
+/*
 	// デバッグ出力
 	show_analytics();
-
+*/
 	return 0;
 }
 
